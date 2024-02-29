766c41bf6e32d304a4ad7b729d0da480f0ba30e1
Sanjib-cmd <sanjibsah919@gmail.com>
2024-02-29 11:15:35 +0530

    initial commit

---
+++ src/Components/Breadcrums/Breadcrum.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Breadcrum.css'
+import arrow_icon from '../Assets/breadcrum_arrow.png'
+
+ const Breadcrum = (props) => {
+    const {product}=props;
+    
+  // if (!product) {
+  //   // Handle the case where product is undefined (e.g., loading state)
+  //   return null; // or return a loading indicator, an empty string, or any fallback UI
+  // }
+  return (
+    <div className='breadcrum'>
+    HOME <img src={arrow_icon} alt=''/> SHOP <img src={arrow_icon} alt=''/> {product?.category} <img src={arrow_icon} alt=''/>{product?.name}
+    {/* HOME <img src={arrow_icon} alt=''/> SHOP <img src={arrow_icon} alt=''/>
+      {product.category && <><img src={arrow_icon} alt=''/> {product.category}</>}
+      {product.name && <><img src={arrow_icon} alt=''/> {product.name}</>} */}
+    </div>
+  )
+}
+export default Breadcrum
